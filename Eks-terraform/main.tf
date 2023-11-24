9c87ff7499e7b66a75735f3a8bd1d249da50106e
viveka1302 <vivekanandyt@gmail.com>
2023-11-24 12:36:31 +0530

    names changed

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -43,7 +43,7 @@ resource "aws_eks_cluster" "example" {
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
-    subnet_ids = values(data.aws_subnets.public)[*].ids
+    subnet_ids = values(data.aws_subnets.public[each.key])
   }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
