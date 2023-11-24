497613108dce6fb59c25dd872a590129fabea2ee
viveka1302 <vivekanandyt@gmail.com>
2023-11-24 13:23:32 +0530

    names changed

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "vivek123-terraform" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    region = ["ap-south-1a","ap-south-1b"]
   }
 }
