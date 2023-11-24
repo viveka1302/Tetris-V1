8c0c0687aac15ff8e4f49643b1c72829ec78df38
viveka1302 <vivekanandyt@gmail.com>
2023-11-24 12:53:50 +0530

    names changed

--- Eks-terraform/main.tf
+++ Eks-terraform/main.tf
@@ -34,7 +34,7 @@ data "aws_subnets" "public" {
 }
 #cluster provision
 resource "aws_eks_cluster" "example" {
-  name     = "EKS_CLOUD"
+  name     = "EKS_CLOUDvivek"
   role_arn = aws_iam_role.example.arn
 
   vpc_config {
